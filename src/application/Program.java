ce16b7b129e27262e2353f77fbbd64b7cb5faf4f
Luiz <lhserafim@gmail.com>
2020-05-17 06:41:16 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String args[]) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        // Programa
+        System.out.println("Enter contract data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        Date date = sdf.parse(sc.next());
+        System.out.print("Contract value: ");
+        double totalValue = sc.nextDouble();
+        System.out.print("Enter number of installments: ");
+        int n = sc.nextInt();
+
+        // Imprimir na tela
+        System.out.print("Installments:");
+
+        sc.close();
+    }
+
+}
